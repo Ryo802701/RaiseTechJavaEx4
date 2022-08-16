7ecf5f70532a69a073c3f4f8f95bc29e2c5dd724
Ryo802701 <rkawa75@gmail.com>
2022-08-16 21:16:05 +0900

    レビュー指摘修正

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,47 @@
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Main {
 
-	private static List<Employee> lstShain;
+	private static List<Employee> lstEmployee;
 
 	public static void main(String[] args) {
 
-		Employee shain1 = new Employee("鈴木", 45, "M", "管理部");
-		Employee shain2 = new Employee("佐藤", 40, "M", "管理部");
-		Employee shain3 = new Employee("山本", 28, "F", "営業部");
-		Employee shain4 = new Employee("中村", 25, "M", "開発部");
-		Employee shain5 = new Employee("松本", 27, "F", "開発部");
-		Employee shain6 = new Employee("渡辺", 30, "M", "営業部");
-		Employee shain7 = new Employee("藤原", 35, "F", "開発部");
+		Employee employee1 = new Employee("鈴木", 45, "M", "管理部");
+		Employee employee2 = new Employee("佐藤", 40, "M", "管理部");
+		Employee employee3 = new Employee("山本", 28, "F", "営業部");
+		Employee employee4 = new Employee("中村", 25, "M", "開発部");
+		Employee employee5 = new Employee("松本", 27, "F", "開発部");
+		Employee employee6 = new Employee("渡辺", 30, "M", "営業部");
+		Employee employee7 = new Employee("藤原", 35, "F", "開発部");
 
 		// 社員のリスト
-		lstShain = new ArrayList<>(Arrays.asList(shain1, shain2, shain3, shain4, shain5, shain6, shain7));
+		lstEmployee = Arrays.asList(employee1, employee2, employee3, employee4, employee5, employee6, employee7);
 
 		// StreamAPI
 		// 開発部の人の名前だけ表示
 		System.out.println("開発部の人は");
-		lstShain.stream().filter(x -> x.getDepartment() == "開発部").forEach(x -> System.out.println(x.getName() + "さん"));
+		lstEmployee.stream().filter(x -> x.getDepartment().equals("開発部"))
+							 .forEach(x -> System.out.println(x.getName() + "さん"));
 
 		System.out.println("---------------------------------------");
 
 		// 年齢の低い順に並べる
 		System.out.println("年齢の低い順");
-		lstShain.stream().sorted((x, y) -> Integer.compare(x.getAge(), y.getAge()))
-				.forEach(x -> System.out.println("名前:" + x.getName() + " 年齢：" + x.getAge()));
+		lstEmployee.stream().sorted((x, y) -> Integer.compare(x.getAge(), y.getAge()))
+							 .forEach(x -> System.out.println("名前:" + x.getName() + " 年齢：" + x.getAge()));
 
 		System.out.println("---------------------------------------");
 
 		// クラスを取り出して文字列を編集する
-		lstShain.stream().map(x -> x.getName() + "さんは" + (x.getSex() == "M" ? "男性" : "女性") + "です。")
-				.forEach(x -> System.out.println(x));
+		lstEmployee.stream().map(x -> x.getName() + "さんは" + (x.getSex().equals("M") ? "男性" : "女性") + "です。")
+							 .forEach(x -> System.out.println(x));
 
 		System.out.println("---------------------------------------");
 
 		// 「藤」がつく人の人数を調べる
-		long cnt = lstShain.stream().filter(x -> x.getName().contains("藤")).count();
+		long cnt = lstEmployee.stream().filter(x -> x.getName().contains("藤"))
+										.count();
 		System.out.println("「藤」がつく人は" + cnt + "人です。");
 
 		System.out.println("---------------------------------------");
@@ -51,7 +52,7 @@ public class Main {
 	}
 
 	private static void searchName(String name) {
-		boolean b = lstShain.stream().anyMatch(x -> x.getName() == name);
+		boolean b = lstEmployee.stream().anyMatch(x -> x.getName().equals(name));
 		System.out.print(name + "さんは");
 		if (b) {
 			System.out.println("います。");
